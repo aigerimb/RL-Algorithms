a1790d5e360060fef5b49e0765c0cebe5cdba367
aigerimb <aigerimb@mail.usf.edu>
2020-09-16 12:08:39 -0400

    A2C

--- A2C_agent.py
+++ A2C_agent.py
@@ -47,18 +47,20 @@ resize = T.Compose([T.ToPILImage(),
 
 class A2C_agent(object):
     
-    def __init__(self, n_episodes, env, device, actor, critic, lr_a, lr_c):
+    def __init__(self, n_episodes, env, device, actor, critic, lr_a, lr_c, gamma, test_interval):
         self.env = env 
         self.n_episodes = n_episodes 
         self.device = device 
         self.actor = actor
         self.critic = critic 
-        self.lr_a = lr_a
-        self.lr_c = lr_c 
+        self.gamma = gamma 
+        self.actor_optim = optim.Adam(actor.parameters(), lr=lr_a)
+        self.critic_optim = optim.Adam(critic.parameters(), lr=lr_c)
+        self.test_interval = test_interval 
         
         
     def act(self, state):
-        
+        a =  self.actor(state)
         probs = F.softmax(self.actor(state))
         # Categorical distirbution takes p vector of probs
         # that specify the probability for each category/action
@@ -68,12 +70,42 @@ class A2C_agent(object):
         
         return action, logprob 
   
-   # def learn(self):
+    def learn(self, logprobs, values, rewards, normalize=True):
+        R = 0
+        returns = []
+        for r in reversed(rewards):
+            R = r + self.gamma*R
+            returns.insert(0, R)
+        
+        returns = torch.tensor(returns, device=self.device)
+        if normalize:
+            returns = (returns - returns.mean()) / returns.std()
+        
+        A = returns-values
+        if normalize:
+            A =  (A - A.mean()) / A.std()
+        actor_loss = -(A.detach() * logprobs).sum()
+        critic_loss = F.smooth_l1_loss(returns, values).sum()
+        self.actor_optim.zero_grad()
+        actor_loss.backward()
+        self.actor_optim.step()
+
+        self.critic_optim.zero_grad()
+        critic_loss.backward()
+        self.critic_optim.step()
+        
+        return actor_loss.item(), critic_loss.item()
         
 
     def train(self):
         env = self.env 
         device = self.device 
+        self.actor.train()
+        self.critic.train()
+        actor_losses = []
+        critic_losses = []
+        ep_rewards = []
+        test_rewards = []
         for i in range(self.n_episodes):
             print("episode: ", i)
             env.reset()
@@ -84,18 +116,18 @@ class A2C_agent(object):
             done = False 
             t = 0
             logprobs = []
-            actions = []
             rewards = []
+            values = []
+            ep_reward = 0
             while done == False:
                 print("t: ", t)
                 action, logprob = self.act(state)
                 v = self.critic(state)
                 # convert action tensor to python scalar and run env
                 _, reward, done, _ = env.step(action.item())
-                reward = torch.tensor([reward], device=device)
-                actions.append(action)
-                logprobs.append([logprob])
-                values.append([v])
+                ep_reward += reward 
+                logprobs.append(logprob)
+                values.append(v)
                 rewards.append(reward)
                 last_screen = current_screen
                 current_screen = get_screen(env, device)
@@ -103,9 +135,49 @@ class A2C_agent(object):
                     next_state = current_screen - last_screen
                 else:
                     next_state = None
-                self.MB.add(state, action, next_state, reward)
                 state = next_state
                 t+=1
                 
-        
-   # def test(self):
+          
+            logprobs = torch.cat(logprobs)
+            values = torch.cat(values).squeeze(-1)
+            
+            # optimize 
+            actor_loss, critic_loss = self.learn(logprobs, values, rewards)
+            actor_losses.append(actor_loss)
+            critic_losses.append(critic_loss)
+            ep_rewards.append(ep_reward)
+            
+            # perform tests 
+            if  i%self.test_interval == 0:
+                test_reward = self.test()
+                test_rewards.append(test_reward)
+                
+        return actor_losses, critic_losses, ep_rewards, test_rewards
+    def test(self):
+        self.actor.eval()
+        env = self.env 
+        device = self.device 
+        env.reset()
+        last_screen = get_screen(env, device)
+        current_screen = get_screen(env, device)
+        state = current_screen - last_screen
+        done = False 
+        t= 0
+        with torch.no_grad():
+            ep_reward = 0
+            while done == False:
+                print("t: ", t)
+                action, logprob = self.act(state)
+                _, reward, done, _ = env.step(action.item())
+                ep_reward += reward 
+                last_screen = current_screen
+                current_screen = get_screen(env, device)
+                if not done:
+                    next_state = current_screen - last_screen
+                else:
+                    next_state = None
+                state = next_state
+                t+=1
+        self.actor.train()
+        return ep_reward
--- a2c.py
+++ a2c.py
@@ -6,10 +6,11 @@ from A2C_agent import get_screen, A2C_agent
 from a2c_net import conv_net, MLP
 
 device = torch.device('cpu')
-hidden_dim = 128
 lr_a = 0.001
-lr_c = 0.001
-n_episodes = 1
+lr_c = 0.0001
+n_episodes = 81
+gamma = 0.99 
+test_interval = 10
 if __name__ == '__main__':
     # allows to access the envionment specific dynamics 
     env = gym.make('CartPole-v0').unwrapped
@@ -21,7 +22,14 @@ if __name__ == '__main__':
     plt.show()
     init_screen = get_screen(env, device)
     actor = conv_net(init_screen.shape[2], init_screen.shape[3], env.action_space.n)
-    critic = MLP(init_screen.shape[2], 1, hidden_dim)
-    agent = A2C_agent(n_episodes, env, device, actor, critic, lr_a, lr_c)
-    agent.train()
+    critic = conv_net(init_screen.shape[2], init_screen.shape[3], 1)
+    agent = A2C_agent(n_episodes, env, device, actor, critic, lr_a, lr_c, gamma, test_interval)
+    actor_loss, critic_loss, train_rewards, test_rewards = agent.train()
+    plt.plot(np.arange(len(train_rewards)), train_rewards, color ='b', label='train rewards')
+    plt.plot(np.arange(0, n_episodes, test_interval), test_rewards, color ='r', label='test rewards')
+    plt.legend()
+    plt.title('A2C Training')
+    plt.xlabel('Episodes')
+    plt.ylabel('Rewards')
+    plt.savefig('A2CTrain.png')
     
